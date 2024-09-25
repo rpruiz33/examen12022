0cedb6cc793a3d25146b4b93c45e52fe44df8047
Your Name <rpruiz33@gmail.com>
2024-09-25 08:11:02 -0300

    second

--- src/modelo/Tarjeta.java
+++ src/modelo/Tarjeta.java
@@ -1,5 +1,33 @@
 package modelo;
 
 public class Tarjeta {
+private String codigo; 
+private Usuario usuario;
 
+
+public String getCodigo() {
+	return codigo;
+}
+public void setCodigo(String codigo) {
+	this.codigo = codigo;
+}
+public Usuario getUsuario() {
+	return usuario;
+}
+public void setUsuario(Usuario usuario) {
+	this.usuario = usuario;
+}
+public Tarjeta(String codigo, Usuario usuario) {
+	super();
+	this.codigo = codigo;
+	this.usuario = usuario;
+}
+
+public boolean equals(Tarjeta tarjeta) {
+	return tarjeta.getCodigo().equals(codigo)&& tarjeta.getUsuario().equals(usuario);
+}
+@Override
+public String toString() {
+	return "Tarjeta [codigo=" + codigo + ", usuario=" + usuario + "]";
+}
 }
--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,5 +1,61 @@
 package modelo;
 
 public class Usuario {
+private int id;
+private long dni;
+private String nombre;
+private String apellido;
+private boolean tieneDescuento;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public long getDni() {
+	return dni;
+}
+public void setDni(long dni) {
+	this.dni = dni;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApellido() {
+	return apellido;
+}
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
+public boolean isTieneDescuento() {
+	return tieneDescuento;
+}
+public void setTieneDescuento(boolean tieneDescuento) {
+	this.tieneDescuento = tieneDescuento;
+}
+public Usuario(int id, long dni, String nomnbre, String apellido, boolean tieneDescuento) {
+	super();
+	this.id = id;
+	this.dni = dni;
+	this.nombre = nomnbre;
+	this.apellido = apellido;
+	this.tieneDescuento = tieneDescuento;
+}
+
+public boolean equals(Usuario usuario) {
+	return usuario.getDni()==this.dni && usuario.getApellido().equals(this.apellido)&& usuario.getNombre().equals(this.nombre)&& 
+			usuario.getId()==this.id && usuario.isTieneDescuento()==this.tieneDescuento;
+}
+@Override
+public String toString() {
+	return "Usuario [id=" + id + ", dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", tieneDescuento="
+			+ tieneDescuento + "]";
+}
+
+
+
 
 }
