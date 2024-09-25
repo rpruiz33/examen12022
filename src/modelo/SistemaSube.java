674a15b87b71f48d88d1bac91ac31774147a557d
Your Name <rpruiz33@gmail.com>
2024-09-25 08:41:09 -0300

    3

--- src/modelo/SistemaSube.java
+++ src/modelo/SistemaSube.java
@@ -1,5 +1,36 @@
 package modelo;
 
+import java.util.List;
+
 public class SistemaSube {
+private List <Usuario> usuarios;
+private List <Tarjeta> tarjetas;
+private List <Viaje> viaje;
+public List<Usuario> getUsuarios() {
+	return usuarios;
+}
+public void setUsuarios(List<Usuario> usuarios) {
+	this.usuarios = usuarios;
+}
+public List<Tarjeta> getTarjetas() {
+	return tarjetas;
+}
+public void setTarjetas(List<Tarjeta> tarjetas) {
+	this.tarjetas = tarjetas;
+}
+public List<Viaje> getViaje() {
+	return viaje;
+}
+public void setViaje(List<Viaje> viaje) {
+	this.viaje = viaje;
+}
+public SistemaSube(List<Usuario> usuarios, List<Tarjeta> tarjetas, List<Viaje> viaje) {
+	super();
+	this.usuarios = usuarios;
+	this.tarjetas = tarjetas;
+	this.viaje = viaje;
+}
+
+
 
 }
--- src/modelo/Viaje.java
+++ src/modelo/Viaje.java
@@ -1,5 +1,60 @@
 package modelo;
 
+import java.time.LocalDateTime;
+
 public class Viaje {
+private Tarjeta tarjeta;
+private String linea;
+private String medio;
+private LocalDateTime fechaHoraViaje;
+private double precio;
+public Tarjeta getTarjeta() {
+	return tarjeta;
+}
+public void setTarjeta(Tarjeta tarjeta) {
+	this.tarjeta = tarjeta;
+}
+public String getLinea() {
+	return linea;
+}
+public void setLinea(String linea) {
+	this.linea = linea;
+}
+public String getMedio() {
+	return medio;
+}
+public void setMedio(String medio) {
+	this.medio = medio;
+}
+public LocalDateTime getFechaHoraViaje() {
+	return fechaHoraViaje;
+}
+public void setFechaHoraViaje(LocalDateTime fechaHoraViaje) {
+	this.fechaHoraViaje = fechaHoraViaje;
+}
+public double getPrecio() {
+	return precio;
+}
+public void setPrecio(double precio) {
+	this.precio = precio;
+}
+public Viaje(Tarjeta tarjeta, String linea, String medio, LocalDateTime fechaHoraViaje, double precio) {
+	super();
+	this.tarjeta = tarjeta;
+	this.linea = linea;
+	this.medio = medio;
+	this.fechaHoraViaje = fechaHoraViaje;
+	this.precio = precio;
+}
+@Override
+public String toString() {
+	return "Viaje [tarjeta=" + tarjeta + ", linea=" + linea + ", medio=" + medio + ", fechaHoraViaje=" + fechaHoraViaje
+			+ ", precio=" + precio + "]";
+}
+
+public boolean equals(Viaje viaje) {
+	return viaje.getFechaHoraViaje().isEqual(this.fechaHoraViaje) && viaje.getMedio().equals(this.medio) && viaje.getPrecio()==this.precio &&
+			viaje.getTarjeta().equals(tarjeta)&& viaje.getLinea().equals(this.linea);
+}
 
 }
